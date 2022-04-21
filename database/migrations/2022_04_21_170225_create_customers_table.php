b9a1d39585995aa671fbf0f2ae95bce6b69e2cac
ManishaDahal <manisha.dahal.3000@gmail.com>
2022-04-21 23:01:15 +0545

    migration for customers

---
+++ database/migrations/2022_04_21_170225_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id('customer_id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('phoneumber', 10);
+            $table->date('dob');
+            $table->string('description')->nullable();
+            $table->boolean('status')->default(1);
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
