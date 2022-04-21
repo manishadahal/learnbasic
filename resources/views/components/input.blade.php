f56294f79b7522046ddbb690c08606cfba0be904
ManishaDahal <manisha.dahal.3000@gmail.com>
2022-04-21 23:00:44 +0545

    Input File

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+    <label for="">{{$label}}</label>
+    <input type="{{$type}}" name="{{$name}}" id="" class="form-control"/>
+    {{-- @error('name')
+    <small class="form-text text-danger">{{ $message}}</small>
+    @enderror --}}
+</div>
