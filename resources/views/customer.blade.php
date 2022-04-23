09d09476ac8340488fce5682e051122d845b6349
ManishaDahal <manisha.dahal.3000@gmail.com>
2022-04-23 21:22:47 +0545

    customer insert page

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts')
 @section('content')
-<form action="{{url('customer.store')}}" method="POST">
+<form action="{{$url}}" method="POST">
     @csrf
     <div class="container">
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" name="name" id="" class="form-control @error('name') is-inavlid @enderror" value="{{ old('name') }}">
+            <input type="text" name="name" id="" class="form-control @error('name') is-inavlid @enderror" value="{{ old('name') ?? $customer->name }}">
             @error('name')
             <small class="form-text text-danger">{{ $message}}</small>
             @enderror
@@ -13,7 +13,7 @@
         <br> <br>
         <div class="form-group">
             <label for="address">Address</label>
-            <input type="text" name="address" id="" class="form-control @error('address') is-inavlid @enderror" value="{{ old('address') }}">
+            <input type="text" name="address" id="" class="form-control @error('address') is-inavlid @enderror" value="{{ old('address') ?? $customer->address }}">
             @error('address')
             <small class="form-text text-danger">{{ $message}}</small>
             @enderror
@@ -21,7 +21,7 @@
         <br> <br>
         <div class="form-group">
             <label for="phonenumber">Phone Number</label>
-            <input type="number" name="phonenumber" id="" class="form-control @error('phonenumber') is-inavlid @enderror" value="{{ old('phonenumber') }}">
+            <input type="number" name="phonenumber" id="" class="form-control @error('phonenumber') is-inavlid @enderror" value="{{ old('phonenumber') ?? $customer->phonenumber }}">
             @error('phonenumber')
             <small class="form-text text-danger">{{ $message}}</small>
             @enderror
@@ -29,7 +29,7 @@
         <br>
         <div class="form-group">
             <label for="dob">Date Of Birth</label>
-            <input type="date" name="dob" id="" class="form-control @error('dob') is-inavlid @enderror" value="{{ old('dob') }}">
+            <input type="date" name="dob" id="" class="form-control @error('dob') is-inavlid @enderror" value="{{ old('dob') ?? $customer->dob}}">
             @error('dob')
             <small class="form-text text-danger">{{ $message}}</small>
             @enderror
@@ -37,7 +37,7 @@
         <br> <br>
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" name="password" id="" class="form-control @error('password') is-inavlid @enderror" value="{{ old('password') }}">
+            <input type="password" name="password" id="" class="form-control @error('password') is-inavlid @enderror">
             @error('password')
             <small class="form-text text-danger">{{ $message}}</small>
             @enderror
